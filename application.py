3addbffec136440ad88778e7dc8885a452146868
skhanbhai <skhanbhai@hotmail.com>
2021-06-13 17:23:25 +0100

    Change the text

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return 'Sup. Subscribe'
+    return 'Hello there good sir. This is my Web Page'
 
 
 if __name__ == "__main__":
