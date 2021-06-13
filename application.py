b34fc83050a7a34daf5447be4e6e8eb5028f32ee
skhanbhai <skhanbhai@hotmail.com>
2021-06-13 17:20:24 +0100

    Change app to applicaton

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     return 'Sup. Subscribe'
 
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
